3704a8c6a6ddbc4410e03041bd1753c52afbde19
Remy Pham <rqpham@ucsc.edu>
2018-02-28 22:10:20 -0800

    improved the index ('/beaches')

--- beaches/views.py
+++ beaches/views.py
@@ -3,8 +3,12 @@ from __future__ import unicode_literals
 
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import loader
+
+from .models import Beach
 
 # Create your views here.
 
 def index(request):
-	return HttpResponse("Hello, world.  You're at the beaches app index.")
+	template = loader.get_template('beaches/index.html')
+	return HttpResponse(template.render())
