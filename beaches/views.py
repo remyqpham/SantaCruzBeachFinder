cd0af2b2408e3ce713119d2bdf2698f8ee52627d
Remy Pham <rqpham@ucsc.edu>
2018-03-06 11:39:46 -0800

    fixed results page, passed images and description previews, formatted nicely

--- beaches/views.py
+++ beaches/views.py
@@ -4,12 +4,18 @@ from __future__ import unicode_literals
 from django.shortcuts import render
 from django.http import HttpResponse, Http404
 from django.template import loader
+from django.template.defaulttags import register
 
 from .models import Beach
 
 import urlparse
 
 num_attributes = 6
+
+@register.filter
+def get_item(dictionary, key):
+    return dictionary.get(key)
+
 # Create your views here.
 
 def index(request):
@@ -29,6 +35,9 @@ def results(request):
 	#template = loader.get_template('beaches/results.html')
 	beaches_list = Beach.objects.order_by('title_text')
 	filtered_beaches_list = Beach.objects.order_by('title_text')
+	filtered_beaches_photos_urls = {}
+	filtered_beaches_desc_previews = {}
+
 	is_pet_friendly = request.GET['is_pet_friendly']
 	is_alcohol_friendly = request.GET['is_alcohol_friendly']
 	is_open_after_sunset = request.GET['is_open_after_sunset']
@@ -52,6 +61,15 @@ def results(request):
 	if has_free_parking=='1':
 		filtered_beaches_list = filtered_beaches_list.filter(has_free_parking=True)
 
+	filtered_beaches_list = filtered_beaches_list.order_by('title_text')
+
+	for beach in filtered_beaches_list:
+		photoURL = str(beach.photo.url)
+		filtered_beaches_photos_urls[beach.id] = photoURL[7:]
+		desc = beach.description_text
+		filtered_beaches_desc_previews[beach.id] = desc[0:100]
+
+
 	context = {
 		'request': request,
 		'beaches_list':beaches_list,
@@ -63,6 +81,8 @@ def results(request):
 		'is_camping_friendly':is_camping_friendly,
 		'is_good_for_surfing':is_good_for_surfing,
 		'has_free_parking':has_free_parking,
+		'filtered_beaches_photos_urls':filtered_beaches_photos_urls,
+		'filtered_beaches_desc_previews':filtered_beaches_desc_previews,
 	}
 	return render(request, 'beaches/results.html', context)
 
